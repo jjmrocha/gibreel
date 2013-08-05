3a1ac21ff7727c19e5165a0fbaae3f1837cac314
Joaquim Rocha <jjmrocha@users.noreply.github.com>
2013-08-05 09:18:51 +0100

    Vers?o distribuida

--- src/g_cache.erl
+++ src/g_cache.erl
@@ -23,13 +23,15 @@
 -define(RECORD_COUNTER, gcache_record_counter).
 -define(NO_INDEX_TABLE, none).
 -define(NO_TASK, none).
+-define(NO_COLUMBO, none).
+-define(CLUSTER_TIMEOUT, 1000).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([start_link/1, stop/1]).
+-export([start_link/1]).
 -export([store/3, get/2, remove/2]).
 -export([size/1]).
 -export([purge/1]).
@@ -37,22 +39,19 @@
 start_link(CacheConfig) when is_record(CacheConfig, cache_config)->
 	gen_server:start_link(?MODULE, [CacheConfig], []).
 
-stop(Server) when is_pid(Server) ->
-	gen_server:cast(Server, {stop}).
-
-store(Server, Key, Value) when is_pid(Server) ->
+store(Server, Key, Value) when is_atom(Server) ->
 	gen_server:cast(Server, {store, Key, Value}).
 
-get(Server, Key) when is_pid(Server) ->
+get(Server, Key) when is_atom(Server) ->
 	gen_server:call(Server, {get, Key}).
 
-remove(Server, Key) when is_pid(Server) ->
+remove(Server, Key) when is_atom(Server) ->
 	gen_server:cast(Server, {remove, Key}).
 
-size(Server) when is_pid(Server) ->
+size(Server) when is_atom(Server) ->
 	gen_server:call(Server, {size}).
 
-purge(Server) when is_pid(Server) ->
+purge(Server) when is_atom(Server) ->
 	gen_server:cast(Server, {purge}).
 
 %% ====================================================================
@@ -60,7 +59,7 @@ purge(Server) when is_pid(Server) ->
 %% ====================================================================
 
 -record(db, {table, index}).
--record(state, {config, db, task}).
+-record(state, {config, db, task, columbo, nodes}).
 
 %% init
 init([CacheConfig]) ->
@@ -68,12 +67,14 @@ init([CacheConfig]) ->
 	Index = create_index(CacheConfig),
 	DB = #db{table=Table, index=Index},
 	Task = start_timer(CacheConfig),
+	{Columbo, Nodes} = start_columbo(CacheConfig),
+	erlang:register(CacheConfig#cache_config.cache_name, self()),
 	error_logger:info_msg("Cache ~p created on [~p]...\n", [CacheConfig#cache_config.cache_name, self()]),
-	{ok, #state{config=CacheConfig, db=DB, task=Task}}.
+	{ok, #state{config=CacheConfig, db=DB, task=Task, columbo=Columbo, nodes=Nodes}}.
 
 %% handle_call
-handle_call({get, Key}, From, State=#state{config=CacheConfig, db=DB}) ->
-	run_get(Key, DB, CacheConfig, From),
+handle_call({get, Key}, From, State=#state{config=CacheConfig, db=DB, nodes=Nodes}) ->
+	run_get(Key, DB, CacheConfig, Nodes, From),
 	{noreply, State};
 
 handle_call({size}, From, State=#state{config=CacheConfig, db=DB}) ->
@@ -81,29 +82,63 @@ handle_call({size}, From, State=#state{config=CacheConfig, db=DB}) ->
 	{noreply, State}.
 
 %% handle_cast
-handle_cast({store, Key, Value}, State=#state{config=CacheConfig, db=DB}) ->
-	run_store(Key, Value, DB, CacheConfig),
+handle_cast({store, Key, Value}, State=#state{config=CacheConfig, db=DB, nodes=Nodes}) ->
+	run_store(Key, Value, DB, CacheConfig, true, Nodes),
 	{noreply, State};
 
-handle_cast({remove, Key}, State=#state{config=CacheConfig, db=DB}) ->
-	run_remove(Key, DB, CacheConfig),
+handle_cast({remove, Key}, State=#state{config=CacheConfig, db=DB, nodes=Nodes}) ->
+	run_remove(Key, DB, CacheConfig, true, Nodes),
 	{noreply, State};
 
 handle_cast({purge}, State=#state{config=CacheConfig, db=DB}) ->
 	run_purge(DB, CacheConfig),
-	{noreply, State};
-
-handle_cast({stop}, State) ->
-	{stop, normal, State}.
-
-%% handle_info
-handle_info({run_purge}, State=#state{config=CacheConfig, db=DB}) ->
-	run_purge(DB, CacheConfig),
 	{noreply, State}.
 
+%% handle_info
+handle_info({cluster_msg, {get, Key, From}}, State=#state{config=CacheConfig, db=DB}) ->
+	run_cluster_get(Key, DB, CacheConfig, From),
+	{noreply, State};
+
+handle_info({cluster_msg, {store, Key, Value}}, State=#state{config=CacheConfig, db=DB}) ->
+	run_store(Key, Value, DB, CacheConfig, false, []),
+	{noreply, State};
+
+handle_info({cluster_msg, {remove, Key}}, State=#state{config=CacheConfig, db=DB}) ->
+	run_remove(Key, DB, CacheConfig, false, []),
+	{noreply, State};
+
+handle_info({run_purge}, State=#state{config=CacheConfig, db=DB}) ->
+	run_purge(DB, CacheConfig),
+	{noreply, State};
+
+handle_info({columbo, {new, _Service, _Ref, Node}}, State=#state{nodes=CurrentNodes}) ->
+	{noreply, State#state{nodes=[Node|CurrentNodes]}};
+
+handle_info({columbo, {remove, _Service, _Ref, Node}}, State=#state{nodes=CurrentNodes}) ->
+	NCurrentNodes = lists:delete(Node, CurrentNodes),
+	{noreply, State#state{nodes=NCurrentNodes}};
+
+handle_info({change_nodes, Nodes}, State=#state{config=CacheConfig, columbo=Columbo}) ->
+	NCacheConfig = CacheConfig#cache_config{nodes=Nodes},
+	case Columbo of
+		?NO_COLUMBO -> 
+			{NColumbo, CurrentNodes} = start_columbo(NCacheConfig),
+			{noreply, State#state{config=NCacheConfig, columbo=NColumbo, nodes=CurrentNodes}};
+		_ -> 
+			columbo:change_nodes(NCacheConfig#cache_config.cache_name, Columbo, Nodes),
+			CurrentNodes= columbo:whereis_service(NCacheConfig#cache_config.cache_name, false),
+			{noreply, State#state{config=NCacheConfig, nodes=CurrentNodes}}
+	end;
+
+handle_info({stop_cache}, State) ->
+	{stop, normal, State}.
+
 %% terminate
-terminate(_Reason, State) ->
-	drop(State#state.db),
+terminate(_Reason, #state{config=CacheConfig, db=DB, task=Task, columbo=Columbo}) ->
+	columbo:delete_notification(CacheConfig#cache_config.cache_name, Columbo),
+	stop_timer(Task),
+	erlang:unregister(CacheConfig#cache_config.cache_name),
+	drop(DB),
 	ok.
 
 %% code_change
@@ -120,7 +155,7 @@ create_table(#cache_config{cache_name=CacheName}) ->
 	ets:insert(Table, {?RECORD_COUNTER, 0}),
 	Table.
 
-create_index(#cache_config{max_size=?NO_MAX_SIZE, prune=?NO_PRUNE}) -> ?NO_INDEX_TABLE;
+create_index(#cache_config{max_size=?NO_MAX_SIZE, purge=?NO_PURGE}) -> ?NO_INDEX_TABLE;
 create_index(#cache_config{cache_name=CacheName}) -> 
 	IndexName = get_index_name(CacheName),
 	ets:new(IndexName, [ordered_set, public]).
@@ -136,27 +171,48 @@ make_name(Name, Posfix) ->
 	ConcatName = LName ++ Posfix,
 	list_to_atom(ConcatName).
 
-start_timer(#cache_config{prune=?NO_PRUNE}) -> ?NO_TASK;
-start_timer(#cache_config{prune=Interval}) ->
+start_timer(#cache_config{purge=?NO_PURGE}) -> ?NO_TASK;
+start_timer(#cache_config{purge=Interval}) ->
 	TimerInterval = Interval * 1000,
 	{ok, Timer} = timer:send_interval(TimerInterval, {run_purge}),
 	Timer.
 
-run_store(Key, Value, DB, CacheConfig) ->
+start_columbo(#cache_config{nodes=?CLUSTER_NODES_LOCAL}) -> {?NO_COLUMBO, []};
+start_columbo(#cache_config{cache_name=CacheName, nodes=ClusterNodes}) ->
+	Fun = fun(Msg) ->
+			CacheName ! {columbo, Msg}
+	end,
+	Nodes = case ClusterNodes of
+		?CLUSTER_NODES_ALL -> [];
+		_ -> ClusterNodes
+	end,
+	{Columbo, CurrentNodes} = columbo:request_notification(CacheName, Fun, Nodes),
+	{Columbo, CurrentNodes}.
+
+run_store(Key, Value, DB, CacheConfig, Notify, Nodes) ->
 	Fun = fun() ->
-			insert(Key, Value, DB, CacheConfig)
+			insert(Key, Value, DB, CacheConfig, Notify, Nodes)
 	end,
 	spawn(Fun).
 
-insert(Key, Value, DB=#db{table=Table, index=Index}, CacheConfig=#cache_config{expire=Expire}) ->
+insert(Key, Value, DB=#db{table=Table, index=Index}, CacheConfig=#cache_config{expire=Expire}, Notify, Nodes) ->
 	Timeout = case Expire of
 		?NO_MAX_AGE -> 0;
 		_ -> current_time() + Expire
 	end,
 	Timestamp = insert_index(Key, Index),
-	case ets:insert(Table, {Key, Value, Timestamp, Timeout}) of
-		true -> update_counter(DB, 1,  CacheConfig);
-		false -> ok
+	case ets:insert_new(Table, {Key, Value, Timestamp, Timeout}) of
+		true -> 
+			update_counter(DB, 1,  CacheConfig),
+			cluster_notify(Notify, Nodes, {store, Key, Value}, CacheConfig);
+		false -> 
+			delete(Key, DB, CacheConfig, false, []),
+			case ets:insert_new(Table, {Key, Value, Timestamp, Timeout}) of
+				true -> 
+					update_counter(DB, 1,  CacheConfig),
+					cluster_notify(Notify, Nodes, {store, Key, Value}, CacheConfig);
+				false -> ok
+			end
 	end.
 
 insert_index(_Key, ?NO_INDEX_TABLE) -> 0;
@@ -178,6 +234,12 @@ update_counter(DB=#db{table=Table}, Inc, CacheConfig=#cache_config{max_size=MaxS
 			end
 	end.
 
+cluster_notify(false, _Nodes, _Msg, _CacheConfig) -> ok;
+cluster_notify(true, [], _Msg, _CacheConfig) -> ok;
+cluster_notify(true, [Node|T], Msg, CacheConfig=#cache_config{cache_name=CacheName}) ->
+	{CacheName, Node} ! {cluster_msg, Msg},
+	cluster_notify(true, T, Msg, CacheConfig).
+
 delete_older(#db{index=?NO_INDEX_TABLE}, _CacheConfig) -> ok;
 delete_older(DB=#db{index=Index}, CacheConfig) ->
 	case ets:first(Index) of
@@ -185,38 +247,48 @@ delete_older(DB=#db{index=Index}, CacheConfig) ->
 		Timestamp -> 
 			case ets:lookup(Index, Timestamp) of
 				[] -> ok;
-				[{_Timestamp, Key}] -> delete(Key, DB, CacheConfig)
+				[{_Timestamp, Key}] -> delete(Key, DB, CacheConfig, false, [])
 			end
 	end.
 
-run_get(Key, DB=#db{table=Table}, CacheConfig=#cache_config{expire=Expire}, From)->
+run_get(Key, DB, CacheConfig, Nodes, From)->
 	Fun = fun() ->
-			Reply = case ets:lookup(Table, Key) of
-				[] -> 
-					case find_value(key, DB, CacheConfig) of
-						{ok, Value} -> {ok, Value};
-						Other -> Other
-					end;
-				[{_Key, Value, _Timestamp, Timeout}] -> 
-					case Expire of
-						?NO_MAX_AGE -> {ok, Value};
-						_ -> 
-							Now = time(),
-							if 
-								Now =< Timeout -> Value;
-								true -> 
-									case find_value(key, DB, CacheConfig) of
-										{ok, Value} -> {ok, Value};
-										Other -> 
-											delete(Key, DB, CacheConfig),
-											Other
-									end
-							end
-					end
-			end,
+			Reply = get(Key, DB, CacheConfig, Nodes, true),
 			gen_server:reply(From, Reply)
 	end,
 	spawn(Fun).
+
+run_cluster_get(Key, DB, CacheConfig, From) ->
+	Fun = fun() ->
+			Reply = get(Key, DB, CacheConfig, [], false),
+			From ! {cluster_msg, Reply}
+	end,
+	spawn(Fun).
+
+get(Key, DB=#db{table=Table}, CacheConfig=#cache_config{expire=Expire}, Nodes, UseCluster) ->
+	case ets:lookup(Table, Key) of
+		[] -> 
+			case find_value(Key, DB, CacheConfig, Nodes, UseCluster) of
+				{ok, Value} -> {ok, Value};
+				Other -> Other
+			end;
+		[{_Key, Value, _Timestamp, Timeout}] -> 
+			case Expire of
+				?NO_MAX_AGE -> {ok, Value};
+				_ -> 
+					Now = current_time(),
+					if 
+						Now =< Timeout -> {ok, Value};
+						true -> 
+							case find_value(Key, DB, CacheConfig, Nodes, UseCluster) of
+								{ok, Value} -> {ok, Value};
+								Other -> 
+									spawn(fun() -> delete(Key, DB, CacheConfig, false, []) end),
+									Other
+							end
+					end
+			end
+	end.
 
 run_size(#db{table=Table}, _CacheConfig, From)->
 	Fun = fun() ->
@@ -228,11 +300,20 @@ run_size(#db{table=Table}, _CacheConfig, From)->
 	end,
 	spawn(Fun).
 
-find_value(_Key, _DB, #cache_config{function=?NO_FUNCTION}) -> not_found;
-find_value(Key, DB, CacheConfig=#cache_config{function=Function}) ->
+find_value(_Key, _DB, #cache_config{function=?NO_FUNCTION}, [], _UseCluster) -> not_found;
+find_value(_Key, _DB, #cache_config{function=?NO_FUNCTION}, _Nodes, false) -> not_found;
+find_value(Key, DB, CacheConfig=#cache_config{function=?NO_FUNCTION}, Nodes, true) -> 
+	cluster_notify(true, Nodes, {get, Key, self()}, CacheConfig),
+	case receive_values(length(Nodes), 0) of
+		{ok, Value} ->
+			spawn(fun() -> insert(Key, Value, DB, CacheConfig, false, []) end),
+			{ok, Value};
+		not_found -> not_found
+	end;
+find_value(Key, DB, CacheConfig=#cache_config{function=Function}, Nodes, _UseCluster) ->
 	try Function(Key) of
 		Result -> 
-			insert(Key, Result, DB, CacheConfig),
+			spawn(fun() -> insert(Key, Result, DB, CacheConfig, true, Nodes) end),
 			{ok, Result}
 	catch 
 		Type:Error -> 
@@ -240,17 +321,30 @@ find_value(Key, DB, CacheConfig=#cache_config{function=Function}) ->
 			error
 	end.	
 
-run_remove(Key, DB, CacheConfig) ->
+receive_values(Size, Size) -> not_found;
+receive_values(Size, Count) ->
+	receive
+		{cluster_msg, {ok, Value}} -> {ok, Value};
+		_ -> receive_values(Size, Count + 1)
+	after ?CLUSTER_TIMEOUT -> not_found
+	end.
+
+run_remove(Key, DB, CacheConfig, Notify, Nodes) ->
 	Fun = fun() ->
-			delete(Key, DB, CacheConfig)
+			delete(Key, DB, CacheConfig, Notify, Nodes)
 	end,
 	spawn(Fun).
 
 run_purge(#db{index=?NO_INDEX_TABLE}, _CacheConfig) -> ok;
-run_purge(DB=#db{index=Index}, CacheConfig) ->
+run_purge(_DB, #cache_config{expire=?NO_MAX_AGE}) -> ok;
+run_purge(DB=#db{index=Index}, CacheConfig=#cache_config{expire=Expire}) ->
 	Fun = fun() ->
-			Now = time(),
-			Match = [{{'$1','$2'},[{'<', '$1', Now}],['$2']}],
+			{Mega, Sec, Micro} = timestamp(),
+			Now = (Mega * 1000000) + Sec,
+			ExpireDate = Now - Expire,
+			NMega = ExpireDate div 1000000,
+			NSec = ExpireDate rem 1000000,
+			Match = [{{'$1','$2'},[{'<', '$1', {{NMega, NSec, Micro}}}],['$2']}],
 			Keys = ets:select(Index, Match),
 			delete_all(Keys, DB, CacheConfig)
 	end,
@@ -258,23 +352,25 @@ run_purge(DB=#db{index=Index}, CacheConfig) ->
 
 delete_all([], _DB, _CacheConfig) -> ok;
 delete_all([Key|T], DB, CacheConfig) ->
-	delete(Key, DB, CacheConfig),
+	delete(Key, DB, CacheConfig, false, []),
 	delete_all(T, DB, CacheConfig).
 
-delete(Key, DB=#db{table=Table}, CacheConfig) ->
-	delete_index(Key, DB),
-	case ets:delete(Table, Key) of
-		true -> update_counter(DB, -1, CacheConfig);
-		false -> ok
-	end.
-
-delete_index(_Key, #db{index=?NO_INDEX_TABLE}) -> ok;
-delete_index(Key, #db{table=Table, index=Index}) ->
+delete(Key, DB=#db{table=Table}, CacheConfig, Notify, Nodes) ->
 	case ets:lookup(Table, Key) of
 		[] -> ok;
 		[{_Key, _Value, Timestamp, _Timeout}] ->
-			ets:delete(Index, Timestamp)
+			delete_index(Timestamp, DB),
+			case ets:delete(Table, Key) of
+				true -> 
+					update_counter(DB, -1, CacheConfig),
+					cluster_notify(Notify, Nodes, {remove, Key}, CacheConfig);
+				false -> ok
+			end
 	end.
+
+delete_index(_Timestamp, #db{index=?NO_INDEX_TABLE}) -> ok;
+delete_index(Timestamp, #db{index=Index}) ->
+	ets:delete(Index, Timestamp).
 
 timestamp() -> os:timestamp().
 
@@ -288,3 +384,7 @@ drop(#db{table=Table, index=Index}) ->
 		none -> ok;
 		_ -> ets:delete(Index)
 	end.
+
+stop_timer(?NO_TASK) -> ok;
+stop_timer(Task) ->
+	timer:cancel(Task).
